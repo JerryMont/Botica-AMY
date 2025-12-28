67991b4c4a77e1135642ab35244dc58a629e4cfe
JerryMont <jerryluismontenegrollamo@gmail.com>
2025-12-27 21:51:51 -0500

    ultima modif

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:5173', 'http://localhost:3000', 'https://logistica-amy.infinityfreeapp.com', 'http://logistica-amy.infinityfreeapp.com'],
 
     'allowed_origins_patterns' => [],
 
